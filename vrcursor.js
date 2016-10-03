8eb068f426c37bfe8b53eb7a8d9d839ba64c3a50
Martin Naumann <martin@geekonaut.de>
2016-10-03 23:18:33 +0200

    Adding GLTFLoader and VrCursor

---
+++ vrcursor.js
@@ -0,0 +1,19 @@
+var VrCursor = function(parent) {
+  var cursor = new THREE.Mesh(
+        new THREE.RingBufferGeometry(0.02, 0.04, 8, 1),
+        new THREE.MeshBasicMaterial({color: 0x008888})
+      ),
+      raycaster = new THREE.Raycaster(parent.position, parent.getWorldDirection(), 0.1, 0.8);
+
+  cursor.position.z = -0.5;
+  this.cursor = cursor;
+
+  parent.add(cursor);
+
+  this.update = function(objects) {
+    raycaster.set(parent.position, parent.getWorldDirection());
+    return raycaster.intersectObjects(objects);
+  };
+
+  return this;
+};
