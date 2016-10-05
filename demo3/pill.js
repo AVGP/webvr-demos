f3810de290d3aad68df016546489b13ab34dc51e
Martin Naumann <martin@geekonaut.de>
2016-10-05 17:42:30 +0200

    Makes Demo3 more Pacman-y

--- demo3/pill.js
+++ demo3/pill.js
@@ -1,10 +1,20 @@
 var Pill = function() {
 
   var root = new THREE.Object3D(),
-      light = new THREE.PointLight(0xffff00, 2.0, 5),
-      pill = new THREE.Mesh(new THREE.SphereGeometry(0.5, 10, 10), new THREE.MeshBasicMaterial({color: 0xffff00, emissive: 0xffff00}));
+      light = new THREE.PointLight(0xffff00, 0.5, 3),
+      pill = new THREE.Mesh(new THREE.SphereGeometry(0.05, 10, 10), new THREE.MeshLambertMaterial({color: 0xffffff}));
 
   root.add(light);
   root.add(pill);
-  return root;
+  return {
+    object: root,
+    setColor: function(colorArray) {
+      pill.material.emissive.fromArray(colorArray);
+      light.color.fromArray(colorArray);
+    },
+    setPosition: function(x, y, z) {
+      pill.position.set(x, y, z)
+      light.position.set(x, y, z)
+    }
+  };
 };
